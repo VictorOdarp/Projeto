b90fb82e49ad3210f7901e6ed9341e32e7ad7ec5
Victor Eduardo <VictorOdarp@gmail.com>
2023-12-23 13:50:29 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -16,7 +16,17 @@ namespace SalesWebMVC.Data
 
         public DbSet<Department> Departament { get; set; } = default!;
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
 
     }
 }
